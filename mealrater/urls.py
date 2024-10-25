7ac366e66d6ffcd8a45cf3083cb68353218f7a5c
allouche mohamed <168310749+allouche45ip@users.noreply.github.com>
2024-10-25 21:58:56 +0200

    basic

---
+++ mealrater/urls.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+   path('admin/', admin.site.urls),
+   path('API/', include('API.urls')),  # تأكد من استخدام اسم التطبيق الصحيح
+]
